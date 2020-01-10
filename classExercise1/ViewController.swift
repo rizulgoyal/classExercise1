eabb9f9f91a4663f3206ca6d0d7f8bb0d606c1d7
rizul <rizulgoyal23@gmail.com>
2020-01-10 18:29:47 -0500

    swipe and move all around done

--- classExercise1/ViewController.swift
+++ classExercise1/ViewController.swift
@@ -11,6 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var viewToMove: UIView!
+    
+    var coordinatecount = 0
+    var swipeRight = false
+    var swipeLeft = false
+    
+    override func viewDidLoad()
+       {
+           super.viewDidLoad()
+           
+           let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipeScreen))
+           swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+           view.addGestureRecognizer(swipeRight)
+           
+           let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipeScreen))
+           swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+           view.addGestureRecognizer(swipeLeft)
+       }
          
       
          override func viewDidAppear(_ animated: Bool) {
@@ -27,10 +44,104 @@ class ViewController: UIViewController {
                 },
                 completion:
                { finished in
+                if self.coordinatecount == 1
+                           {
+                               self.moveLeft()
+                           }
                 
                 })
         
         }
+    
+    func moveRight()
+    {
+        UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear, animations:
+                    {
+                        let guide = self.view.safeAreaLayoutGuide
+                        let width = guide.layoutFrame.size.width
+                        self.viewToMove.frame.origin.x = width - self.viewToMove.frame.size.height
+    
+                    }, completion: { finished in
+                        if self.coordinatecount == 0{
+                        self.moveUp()
+                        }
+                        else
+                        {
+                        self.moveDown()
+                        }
+                    })
+    }
+    
+    //function to move up
+    func moveUp()
+    {
+        
+        UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear, animations:
+            {
+                self.viewToMove.frame.origin.y = self.view.safeAreaInsets.bottom
+            },
+            completion: { finished in
+                if self.coordinatecount == 0{
+                self.moveLeft()
+                }
+                else
+                {
+                self.moveRight()
+                }
+            })
+    }
+    
+    //function to move left
+    func moveLeft()
+    {
+        
+        self.viewToMove.frame.origin.x = self.view.bounds.maxX - 50
+        
+        UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear, animations:
+            {
+                        self.viewToMove.frame.origin.x =  self.view.safeAreaInsets.left + 10
+                        
+        
+            },
+                       completion: { finished in
+                        if self.coordinatecount == 0
+                       {
+                       self.moveDown()
+                       }
+                        else
+                        {
+                            if self.swipeRight == true
+                            {
+                            self.moveUp()
+                            }
+                            
+                        }
+                    
+            })
+        }
+    
+    @objc func swipeScreen(gesture: UISwipeGestureRecognizer) {
+        
+        let swipeGesture = gesture as UISwipeGestureRecognizer
+        switch swipeGesture.direction {
+        case UISwipeGestureRecognizer.Direction.right:
+            swipeRight  = true
+            swipeLeft = false
+            print("right swipe")
+            self.coordinatecount = 0
+            self.moveRight()
+        case UISwipeGestureRecognizer.Direction.left:
+            swipeLeft = true
+            swipeRight = false
+            print("left swipe")
+            self.coordinatecount = 1
+            self.moveUp()
+        default:
+            break
+        }
+    }
+    
+    
     
     
 
